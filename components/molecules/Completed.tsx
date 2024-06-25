3ae092f94dcc3f1c0490aeb7aeef22b6bb946937
Santiago Crippa <santicrippa@gmail.com>
2024-06-25 00:30:48 -0300

    Rendering ui with data

--- components/molecules/Completed.tsx
+++ components/molecules/Completed.tsx
@@ -19,15 +19,16 @@ export default function Completed() {
         </View>
         <View className="w-[311px] bg-[#00B47D] rounded">
           <View className="w-full ml-4 mt-[9px] border-yellow-500 text-white">
-            <Text className="text-white font-bold text-base leading-[19.2px]">Pressure Wash</Text>
-            <Text className="text-white text-xs leading-[14.4px]">Aqua Pressure and Soft Wash LLC</Text>
-            <Text className="text-white text-xs font-bold leading-[14.4px]">404-964-9329</Text>
+            <Text className="text-white font-bold text-base leading-[19.2px]">{data?.calendar[0].actions[0].name}</Text>
+            <Text className="text-white text-xs leading-[14.4px]">{data?.calendar[0].actions[0].vendor?.vendorName}</Text>
+            <Text className="text-white text-xs font-bold leading-[14.4px]">{data?.calendar[0].actions[0].vendor?.phoneNumber}</Text>
             <View className="mt-[10px]">
               <View className="flex flex-row items-center gap-x-[2px]">
                 <MapPinIcon width={10} height={10} color='#FFFFFF' />
-                <Text className="text-white text-xs leading-[14.4px]">4332 Northeast 22nd Avenue</Text>
+                <Text className="text-white text-xs leading-[14.4px]">{data?.customer.street}</Text>
               </View>
-              <Text className="text-white text-xs font-normal leading-[14.4px]">Completed</Text>
+              <Text className="text-white text-xs font-normal leading-[14.4px]">{data?.status}</Text>
+
             </View>
           </View>
         </View>
