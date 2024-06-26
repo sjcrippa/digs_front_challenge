769b68acb18577890347de17dcf5cf696a8daf16
Santiago Crippa <santicrippa@gmail.com>
2024-06-26 16:45:51 -0300

    Adding background color logic and Loading

---
+++ components/Loading.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Loading() {
+  return (
+    <View className='w-full h-screen flex justify-center place-items-center -translate-y-24'>
+      <Text className="text-xl font-bold">Loading...</Text>
+    </View>
+  )
+}
